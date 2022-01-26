ea453be8aa5b5857f3ad6da38cc8a58d336a49dc
ahsansaif47 <ahsan_saif47@hotmail.com>
2022-01-27 00:09:01 +0500

    tf variable code updated

--- tf_variables.py
+++ tf_variables.py
@@ -1,3 +1,5 @@
+from re import X
+from numpy import gradient
 import tensorflow as tf
 
 # variable comprising of zeros..
@@ -5,3 +7,24 @@ var = tf.Variable([0, 0, 0])
 
 # assigning it some value..
 var.assign([1, 2, 3])
+
+
+# differenciation..
+# y = f(x)
+# calculating gradients of model loss/error with model weights.
+
+x = tf.Variable(1.0)
+
+
+def f(x):
+    y = x**2 + 2*x - 5
+    return y
+
+
+# now calculating derivative of function y wrt x
+with tf.GradientTape() as tape:
+    y = f(x)
+    print("Value of y is: {}".format(y))
+
+der_y = tape.gradient(y, x)
+print("First derivative of y is: {}".format(der_y))
